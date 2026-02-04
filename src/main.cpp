bdeff81caf9dae50c1cd4397f0e41829462b38da
Alter <148206903+pratikacharya1234@users.noreply.github.com>
2026-02-03 22:53:41 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+int main()
+{
+    //instantiating random number generator
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    return 0;
+}
